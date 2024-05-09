533a327235d444ec19974e9504bf3757a8e2277a
prashantraorane <142421656+prashantraorane@users.noreply.github.com>
2024-05-09 19:39:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -271,7 +271,7 @@ if prompt := st.chat_input("Your question"): # Prompt for user input and save to
     #     content=prompt,
     # ))
     print("question--------------------->",prompt)
- -
+ 
 for message in st.session_state.messages: # Display the prior chat messages
     with st.chat_message(message["role"]):
         st.write(message["content"])
